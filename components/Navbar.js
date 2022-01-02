76bbabc153742fe9c2d43a6007ebb77a3a02387a
Ashwani <ashwanikumar150798@gmail.com>
2022-01-02 19:41:10 +0530

    Navbar styles completed

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,12 +3,17 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="sm:container mx-auto border-b p-4 flex justify-between">
       <Link href='/' passHref>
-        <div className="sm:container mx-auto border-b p-4 text-xl font-semibold cursor-pointer">
+        <div className="text-xl font-semibold cursor-pointer">
           Blog Title
         </div>
       </Link>
+      <ul className="flex items-center">
+        <li className="ml-2 rounded px-3 py-1 hover:bg-slate-900 hover:text-white">
+          <Link href='/login'><a>Login</a></Link>
+        </li>
+      </ul>
     </nav>
   )
 }
