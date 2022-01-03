ea230d2967047de5c3384a399aa1f4e37e86ac12
Ashwani <ashwanikumar150798@gmail.com>
2022-01-03 10:50:57 +0530

    Inintail Login page setup

--- pages/login.js
+++ pages/login.js
@@ -2,8 +2,9 @@
 
 export default function Login() {
   return (
-    <div className="flex justify-center bg-gray-300">
-      <h1>Login page</h1> 
+    <div className="h-full flex justify-center items-center">
+      <button className="px-3 rounded py-1 bg-slate-200 hover:bg-slate-900 hover:text-white">Login with Github</button> 
     </div>
   )
 }
+ 
